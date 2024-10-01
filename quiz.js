f76ab9cb9bbf55851f5790de647db7d21266aca4
Macray Billy <166559496+P1pp3n@users.noreply.github.com>
2024-10-01 11:58:27 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ function checkAnswer() {
     //sets the correct answer
     let correctAnswer="4";
     //gets the value of the selected answer
-    userAnswer=document.querySelector('input[name="quiz"]:checked').value;
+    let userAnswer=document.querySelector('input[name="quiz"]:checked').value;
     //compare user's answer with the correct answer
     if(userAnswer===correctAnswer){
         document.getElementById("feedback").textContent="Correct! Well done.";
