c21ac63335465dd2374895c514c1401442c57604
Macray Billy <166559496+P1pp3n@users.noreply.github.com>
2024-10-01 12:20:10 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ function checkAnswer() {
     //gets the value of the selected answer
     let userAnswer = document.querySelector('input[name="quiz"]:checked');
     
-    if(userAnswer.value === correctAnswer){
+    if (userAnswer.value === correctAnswer){
         document.getElementById("feedback").textContent="Correct! Well done.";
     }else{
         document.getElementById("feedback").textContent="That's incorrect. Try again!";
